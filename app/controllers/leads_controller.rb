5ee904332c9a7716bd4649d20fde96c669e04d34
Khan <Talhaakhan@pm.me>
2022-10-01 13:45:46 -0700

    views and tables added

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,4 +1,5 @@
 class LeadsController < ApplicationController
+
   def index
   end
 
@@ -10,11 +11,28 @@ class LeadsController < ApplicationController
   end
 
   def create
+    @lead = Lead.new(lead_params)
+    @lead.user = current_user
+    if current_user.agent? 
+      if @lead.save
+      flash[:notice]= "Your sales is processed for authorization"
+      redirect_to leads_path
+    else
+      flash[:notice]= "Your lead has some issues"
+      render :new, status: :unprocessable_entity
+    end
+  end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def lead_params
+    params.require(:lead).permit!
   end
 end
