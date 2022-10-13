bce2760c6d38d173db49c8e9b758c880a5aebfa4
Khan <Talhaakhan@pm.me>
2022-10-12 17:17:40 -0700

    emails ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'billssquare.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'billssquare.herokuapp.com', :protocol => 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
