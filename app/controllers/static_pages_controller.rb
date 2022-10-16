c19c5b9cc7dca49f1386c5a12af26636d0cdeda6
Khan <Talhaakhan@pm.me>
2022-10-15 23:02:57 -0700

    fixing dasboard views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,6 +15,21 @@ class StaticPagesController < ApplicationController
   
   end
 
-  def landing_page
+  def stats
+    @charged_sales_last_month = current_user.leads.where(created_at: 2.months.ago..1.month.ago, status: 'charged').sum(:sale_amount).to_f
+    @charged_sales_this_month = current_user.leads.where(created_at: 1.months.ago..Time.now, status: 'charged').sum(:sale_amount)
+    @difference_in_sales= (@charged_sales_this_month.to_f - @charged_sales_last_month.to_f)
+    @fraction_change = @difference_in_sales/@charged_sales_last_month
+    @percentage_change = @fraction_change * 100
+    
+
+    @chargebacks_this_month = current_user.leads.where(created_at: 1.month.ago..Time.now, status: 'chargeback').count.to_f
+    @chargebacks_last_month =  current_user.leads.where(created_at: 2.months.ago..1.month.ago, status: 'chargeback').count.to_f
+    @charged_sales_count = current_user.leads.where(created_at: 1.month.ago..Time.now, status: 'charged').count.to_f
+    @charged_sales_last_count = current_user.leads.where(created_at: 2.months.ago..1.month.ago, status: 'charged').count.to_f
+
+
+    @percentage_chargebacks_in_this_month = (@chargebacks_this_month/@charged_sales_count) * 100 
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :leads
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'static_pages/dashboard'
-  get 'static_pages/landing_page'
+  get 'stats', to: "static_pages#stats"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
